75ade124d3996c6b6c9b9b223653cab1c8de1775
SkyFoxx <skyfoxx@sacred.finance>
2022-11-22 15:58:48 +1100

    chore: update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Sacred.Finance',
+    title: 'Sacred Finance',
     tagline: 'Liberating Speech',
     url: 'https://sacred.finance',
     baseUrl: '/',
